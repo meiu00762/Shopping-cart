fda2a6fa68046d99ba3a2d596607f545e67661d7
meiu00762 <meiu00762@gmail.com>
2019-08-28 14:55:30 +0530

    2 files modified

--- src/app/clothes.service.ts
+++ src/app/clothes.service.ts
@@ -10,7 +10,7 @@ export class ClothesService {
     constructor() {
         this.products = [
             { id: '2001', name: 'Casuals', price: 650, photo: 'https://ae01.alicdn.com/kf/HTB1BNPCL3HqK1RjSZFEq6AGMXXan/2019-New-Solid-color-T-Shirt-pants-men-Mens-Black-And-White-100-cotton-T-shirts.jpg' },
-            { id: '2002', name: 'Formals', price: 750, photo: 'http://myfaashions.com/wp-content/uploads/2016/10/5-2.jpg' },
+            { id: '2002', name: 'Formals', price: 750, photo: 'https://cf.ltkcdn.net/mens-fashion/images/std/231576-300x450-businessman.jpg' },
             { id: '2003', name: 'Jeans', price: 950, photo: 'https://img.tatacliq.com/images/i3/252Wx374H/MP000000004071931_252Wx374H_20181214141410.jpeg' }
         ];
     }
