01f60f392d783a0be42bee2e8034f508fc5913c6
meiu00762 <meiu00762@gmail.com>
2019-08-28 10:19:32 +0530

    3 files added

---
+++ src/app/shop/product-item-clothes/product-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Product } from '../../shop/entity/product.entity';
+import { ProductService } from '../../product.service';
+
+@Component({
+  selector: 'app-product-item',
+	templateUrl: 'product-item.component.html',
+  styleUrls: ['./product-item.component.scss']
+})
+
+export class ProductComponent implements OnInit {
+
+	private products: Product[];
+
+	constructor(
+		private productService: ProductService
+	) { }
+
+	ngOnInit() {
+		this.products = this.productService.findAll();
+	}
+
+
+}
