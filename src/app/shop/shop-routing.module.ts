6579babc472eff8dbffb944963ec381dadd077e0
meiu00762 <meiu00762@gmail.com>
2019-08-29 09:11:46 +0530

    2 files modified, 18 deleted

--- src/app/shop/shop-routing.module.ts
+++ src/app/shop/shop-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {OrderComponent} from './order/order.component';
 import {ProductComponent} from './product-item/product-item.component';
-import {ProductManagerComponent} from './product-manager/product-manager.component';
-import {ShopZoneComponent} from './shop-zone/shop-zone.component';
 import {ClothesComponent } from './product-item-clothes/product-item.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'store', pathMatch: 'full'},
   {path: 'store', component: ProductComponent, pathMatch: 'full'},
-  {path: 'order', component: OrderComponent},
   { path: 'clothes', component: ClothesComponent },
-  {path: 'products-manager', component: ProductManagerComponent}
 ];
 
 @NgModule({
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductListComponent } from './product-list/product-list.component';
+
 import { ProductComponent } from './product-item/product-item.component';
 import { CartComponent } from './cart/cart.component';
-import { SearchComponent } from './search/search.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
-import { OrderComponent } from './order/order.component';
-import { ProductManagerComponent } from './product-manager/product-manager.component';
 import {ShopRoutingModule} from './shop-routing.module';
-import { ShopZoneComponent } from './shop-zone/shop-zone.component';
-import { CategoryFilterComponent } from './category-filter/category-filter.component';
 import {ClothesComponent } from './product-item-clothes/product-item.component';
 
 @NgModule({
   declarations: [
-    ProductListComponent, ProductComponent, CartComponent,
-    SearchComponent, SideBarComponent, OrderComponent, ProductManagerComponent, ShopZoneComponent, CategoryFilterComponent,ClothesComponent
+    ProductComponent, CartComponent,
+     SideBarComponent,ClothesComponent
   ],
   imports: [
     CommonModule, ShopRoutingModule
