019fce1cba372cdc49277d0dab2f45bae21241a2
meiu00762 <meiu00762@gmail.com>
2019-08-29 12:15:35 +0530

    1 file modified, 3 deleted

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HomeLayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
-import { InformationComponent } from './information/information.component';
 
 @NgModule({
-  declarations: [HomeLayoutComponent, LoginComponent, AboutComponent, InformationComponent],
+  declarations: [HomeLayoutComponent, LoginComponent, AboutComponent],
   imports:      [CommonModule],
   exports:      []
 })
