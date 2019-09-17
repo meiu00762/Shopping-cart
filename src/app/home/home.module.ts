b26881247b72c790157d7bf5611cc459d1b9235d
meiu00762 <meiu00762@gmail.com>
2019-09-17 10:18:12 +0530

    4 files modified

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HomeLayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from '..//app-routing.module';
 
 @NgModule({
   declarations: [HomeLayoutComponent, LoginComponent, RegisterComponent],
-  imports:      [CommonModule],
+  imports:      [CommonModule, AppRoutingModule],
   exports:      []
 })
 export class HomeModule { }
