f00dfb4f0adab10e11707deb935da67360b969a3
meiu00762 <meiu00762@gmail.com>
2019-10-17 10:07:53 +0530

    11 files modified

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -4,6 +4,7 @@ import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
+
 @NgModule({
   declarations: [LayoutComponent, HeaderComponent, FooterComponent],
   imports: [
