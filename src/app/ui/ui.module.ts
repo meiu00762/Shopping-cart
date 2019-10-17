e5337a219c372df18a7698634ee4048e3b1213c6
meiu00762 <meiu00762@gmail.com>
2019-10-17 10:11:55 +0530

    1 file modified

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import {ShopRoutingModule} from '.././shop/shop-routing.module';
 
 
 @NgModule({
   declarations: [LayoutComponent, HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule, ShopRoutingModule
   ],
   exports: [LayoutComponent]
 })
